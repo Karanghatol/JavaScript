94fb0135bb1b777dfad90326b21f1e10cf1a5a1b
karan keshav ghatol <122148804+Karanghatol@users.noreply.github.com>
2023-12-16 01:02:14 +0530

    Add files via upload

---
+++ string_templates.js
@@ -0,0 +1,19 @@
+{
+
+    //this (``) is used
+    const name = "karan ghatol"
+    const age = 19
+
+    // ${} to directly include variables in to string
+    console.log(`name : ${name} \nage : ${age}`);
+
+    //to write multiline string
+    let str = `
+        this 
+        is
+        multiline 
+        string
+    `
+    console.log(str);
+
+}
