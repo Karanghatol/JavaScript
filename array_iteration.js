0043e5d399a94177adfb26135e37abb714bcc1c2
karan keshav ghatol <122148804+Karanghatol@users.noreply.github.com>
2023-12-18 00:59:01 +0530

    Add files via upload

---
+++ array_iteration.js
@@ -0,0 +1,23 @@
+{
+    // array iteration is the process, 
+    // whent we reach each element of an array 
+    // it 
+
+    // for loop
+    let elem = 10
+    for (i = 1; i <= elem; i++) {
+        console.log(i)
+    }
+    // forEach loop
+    let car = ['BMW', 'Rover', 'Audi']
+    car.forEach(function (bmw) {
+        console.log(bmw)
+    });
+    // for of loop
+    let Bike = ['kawasaki','BMW-Bike 125354AdDE','Royal Enfield']
+    for (const twowheel of Bike) {
+        console.log(twowheel)
+    }
+
+
+}
