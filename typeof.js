a37f2a2f599dc5e8ad7951f08d42a877213e5380
karan keshav ghatol <122148804+Karanghatol@users.noreply.github.com>
2023-12-19 20:42:14 +0530

    Add files via upload

---
+++ typeof.js
@@ -0,0 +1,23 @@
+{
+    /**
+     * it is used to know the type of variable 
+    */
+
+    let num = 10
+    let obj = [10, 20, 30] // obj
+    let arr = new Array([10, 20, 30]) // object
+    let str = 'string'
+    let bool = true
+    let undef;
+    let n = null
+    function func() {
+        // code 
+    }
+
+    console.log(typeof (num));
+    console.log(typeof (arr));
+    console.log(typeof (obj));
+    console.log(typeof (str));
+    console.log(typeof (bool));
+    console.log(typeof func);
+}
