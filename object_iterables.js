a8329b751ab1aed3db07aebb7757042fa0f99339
karan keshav ghatol <122148804+Karanghatol@users.noreply.github.com>
2023-12-31 23:00:48 +0530

    Add files via upload

--- object_iterables.js
+++ object_iterables.js
@@ -1,28 +1,57 @@
 {
     for (const x of "Hello World!") {
-        console.log(x);
+        // console.log(x);
     }
-    /**
-     * output
-     * 
-    H
-    e
-    l
-    l
-    o
-    
-    W
-    o
-    r
-    l
-    d
-    !
-     */
 
     {
-        // array
-        for (let x of [1, 'a', true, { key: "value" }]) {
-            console.log(x);
+        function addTen() {
+            let n = 0
+            return {
+                get next() {
+                    n += 10
+                    return {
+                        val: n,
+                        done: false
+                    }
+                }
+            }
         }
+        // create an object of addTen
+        let obj = addTen()
+        console.log(obj.next.val); // 10
+        console.log(obj.next.val); // 20
+        console.log(obj.next.val); // 30
+        console.log(obj.next.val); // 40
+        console.log(obj.next.val); // 50
+        // n will increase by +10 every time it called
+    }
+
+    {
+        // creataing an Object
+        Mynum = {}
+
+        // make it iterable
+        Mynum[Symbol.iterator] = function () {
+            let n = 0;
+            done = false;
+            return {
+                get next() {
+                    n += 10
+                    if(n === 100) {
+                        done = true
+                    }
+                    return {
+                        vlaue:n,
+                        done: done
+                    }
+                }
+            }
+        }
+        
+        let text = ""
+        for (let num of Mynum) {
+            text += num + '\n'
+        }
+        console.log(text);
     }
 }
