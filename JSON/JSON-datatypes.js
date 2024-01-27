f5e51506eeea2cd8105b1b0f4ef2b9d4f61b0f17
karan keshav ghatol <122148804+Karanghatol@users.noreply.github.com>
2024-01-27 22:09:38 +0530

    Add files via upload

---
+++ JSON/JSON-datatypes.js
@@ -0,0 +1,30 @@
+{
+    // datatypes in json : 
+
+    // String 
+    let str = {
+        "string": "Stringvalue",
+    };
+    // number 
+    let num = {
+        "number": 20,
+    };
+    // object 
+    let obj = {
+        "object": {"key":"value"},
+        "object2": {"key":10},
+    };
+    // boolean
+    let bool = {
+      "boolean":true, 
+      "boolean2":false, 
+    };
+    // null
+    let nul = {
+      "null":null, 
+    };
+    // array
+    let arr = {
+        "arr":["a","b",1,null,true],
+    }
+}
