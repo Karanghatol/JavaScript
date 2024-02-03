230014389ed0876393d1a2e2310c04c3cdaa9b40
karan keshav ghatol <122148804+Karanghatol@users.noreply.github.com>
2024-02-03 23:25:58 +0530

    JSON-php.php

--- JSON/JSON-php.php
+++ JSON/JSON-php.php
@@ -7,7 +7,7 @@ $myobj = array(
     "projects" => null,
 );
 // stdClass
-$myjson = json_encode($myobj, JSON_FORCE_OBJECT);  // shows index
+$myjson = json_encode($myobj, JSON_FORCE_OBJECT | JSON_PRETTY_PRINT);  // shows index, most readable form
 file_put_contents("C:\\Users\\lenovo\\Documents\\JavaScript\\JSON\\JSON-PHP\\JSON-php.txt", $myjson);  // Escape backslashes
 $fileContent = file_get_contents("C:\\Users\\lenovo\\Documents\\JavaScript\\JSON\\JSON-PHP\\JSON-php.txt");  // Escape backslashes
 $decodedData = json_decode($fileContent, true);  // Set the second parameter to true to get an associative array
